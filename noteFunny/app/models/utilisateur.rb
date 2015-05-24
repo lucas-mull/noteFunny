ed19d21ed94fd1c14f29fec6fadb06585a701ea0
Lucas Mull <lucas.mull@hotmail.fr>
2015-05-24 20:30:14 +0200

    Version 1.0

--- noteFunny/app/models/utilisateur.rb
+++ noteFunny/app/models/utilisateur.rb
@@ -1,8 +1,11 @@
 class Utilisateur < ActiveRecord::Base
+
+	has_secure_password
+
 	validates :nom, :presence => true
 	validates :prenom, :presence => true
 	validates :email, :presence => true, :uniqueness => true
-	validates :password, :presence => true
+	validates :password, :presence => true, :if => :password
 
 	def fullname
 		return prenom.to_s + " " + nom.to_s
--- noteFunny/db/migrate/20150507095047_utilisateurs.rb
+++ noteFunny/db/migrate/20150507095047_utilisateurs.rb
@@ -4,13 +4,14 @@ class Utilisateurs < ActiveRecord::Migration
   		t.column :nom, :string, :limit => 50, :null => false
   		t.column :prenom, :string, :limit => 50, :null => false
   		t.column :email, :string, :limit => 100, :null => false
-  		t.column :password, :string, :limit => 100, :null => false
+  		t.column :password_digest, :string, :null => false
   	end
   end
 
 
   def self.down
   	drop_table :utilisateurs
+  end
 end
 
 
